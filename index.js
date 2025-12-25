d2ad2e2224289683e050a237a52cd0893dededc7
KUSAL S D <156213745+ksusXLabs@users.noreply.github.com>
2025-12-25 23:15:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const app = express();
 __path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require("./pair");
-require("events").EventEmitter.defaultMaxListeners = 500;
-app.use("/code", code);
 
-app.use("/", async (req, res, next) => {
-  res.sendFile(__path + "/pair.html");
-});
+let code = require("./pair");
+
+require("events").EventEmitter.defaultMaxListeners = 500;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/code", code);
+
+app.use("/", async (req, res) => {
+  res.sendFile(__path + "/pair.html");
+});
+
 app.listen(PORT, () => {
-  console.log(`⏩ Server running on http://localhost:` + PORT);
+  console.log("⏩ IZUMI LITE running on port " + PORT);
 });
 
 module.exports = app;
