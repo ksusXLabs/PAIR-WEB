ad6ab51e0baa6b1d5e9103faac5bd400e56c5122
KUSAL S D <156213745+ksusXLabs@users.noreply.github.com>
2025-12-26 00:32:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,36 +2,19 @@ const express = require("express");
 const app = express();
 __path = process.cwd();
 const bodyParser = require("body-parser");
-const fs = require("fs");
 const PORT = process.env.PORT || 8000;
-
 let code = require("./pair");
-
 require("events").EventEmitter.defaultMaxListeners = 500;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Existing /code route
 app.use("/code", code);
 
-// Load plugins dynamically
-const pluginFiles = fs.readdirSync(__path + "/plugins").filter(f => f.endsWith(".js"));
-pluginFiles.forEach(file => {
-  const plugin = require(__path + "/plugins/" + file);
-  if(plugin.route && plugin.method && plugin.handler) {
-    app[plugin.method](plugin.route, plugin.handler);
-    console.log(`✅ Plugin loaded: ${file} -> ${plugin.route}`);
-  }
-});
-
-// Serve frontend
-app.use("/", async (req, res) => {
+app.use("/", async (req, res, next) => {
   res.sendFile(__path + "/pair.html");
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-  console.log("⏩ IZUMI LITE running on port " + PORT);
+  console.log(`⏩ Server running on http://localhost:` + PORT);
 });
 
 module.exports = app;
